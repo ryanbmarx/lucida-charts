e9ef0509b48aa1a9356029d350249951d12e5c5e
Ryan Marx <rmarx@gannett.com>
2020-04-27 16:05:50 -0500

    better watch methods

--- scripts/watch-html.js
+++ scripts/watch-html.js
@@ -4,7 +4,7 @@ const mkdirp = require("mkdirp");
 const fs = require("fs");
 const colors = require("colors");
 
-chokidar.watch("./src/**/*.html").on("change", (srcPath) => {
+chokidar.watch(["./src/**/*.html", "./src/**/*.csv", "./src/**/*.css"]).on("change", (srcPath) => {
   console.log(colors.green(`+++ Changes to ${srcPath}`));
   const publicPath = path.dirname(srcPath).replace("src", "public");
   const fileName = path.basename(srcPath);
