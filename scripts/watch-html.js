c0060d090524bbdad10fbadbe577a58fa8dd0e48
Ryan Marx <rmarx@gannett.com>
2020-04-27 11:19:05 -0500

    watch and copy html

---
+++ scripts/watch-html.js
@@ -0,0 +1,17 @@
+const chokidar = require("chokidar");
+const path = require("path");
+const mkdirp = require("mkdirp");
+const fs = require("fs");
+const colors = require("colors");
+
+chokidar.watch("./src/**/*.html").on("change", (srcPath) => {
+  console.log(colors.green(`+++ Changes to ${srcPath}`));
+  const publicPath = path.dirname(srcPath).replace("src", "public");
+  const fileName = path.basename(srcPath);
+
+  mkdirp(publicPath)
+    .then((m) => {
+      fs.copyFile(srcPath, `${publicPath}/${fileName}`, (err) => {});
+    })
+    .catch((e) => console.log(colors.red(`${e}`)));
+});
