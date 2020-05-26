01fd6a7355c5a9eabe7903496f3d2ee763a68c62
Ryan Marx <ryanbmarx@gmail.com>
2020-05-25 21:19:30 -0500

    FIre maps

--- scripts/main-index.js
+++ scripts/main-index.js
@@ -1,12 +1,13 @@
 const dirTree = require("directory-tree");
-const fs=require("fs");
+const fs = require("fs");
 
-function generateIndex(){    
-    const filteredTree = dirTree("./public/narratives", { extensions: /\.html/ });
-    fs.writeFile("./src/structure.json", JSON.stringify(filteredTree), err=> {if (err) throw err})
+function generateIndex() {
+  const filteredTree = dirTree("./public/narratives", { extensions: /\.html/ });
+  fs.writeFile("./src/structure.json", JSON.stringify(filteredTree), err => {
+    if (err) throw err;
+  });
 }
 
-
-generateIndex()
+generateIndex();
 
 module.exports = generateIndex;
